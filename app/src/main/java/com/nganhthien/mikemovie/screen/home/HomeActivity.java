2bbb631b68ae2672fa57cea200f26810f502c422
Mike <ng.anhthien@gmail.com>
2018-06-29 17:20:35 +0700

    Splash Screen and Toolbar-XML

--- app/src/main/java/com/nganhthien/mikemovie/screen/home/HomeActivity.java
+++ app/src/main/java/com/nganhthien/mikemovie/screen/home/HomeActivity.java
@@ -1,15 +1,16 @@
-package com.nganhthien.mikemovie.screen.main;
+package com.nganhthien.mikemovie.screen.home;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.nganhthien.mikemovie.R;
 import com.nganhthien.mikemovie.screen.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
